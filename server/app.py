ca2922f05a801622b471ca2a8852976ea4905d04
Perpetual Akinyi <you@example.com>
2025-01-25 12:32:20 +0300

    made a relationship and a serialization rule for Restaurant

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
 from flask import Flask, request, make_response
 from flask_restful import Api, Resource
+from models import db, Restaurant, RestaurantPizza, Pizza
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -23,6 +24,17 @@ api = Api(app)
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+@app.route("/restaurants")
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return {"restaurants": [restaurant.to_dict() for restaurant in restaurants]}
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -21,8 +21,12 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="restaurant", cascade= "all, delete, delete-orphan")
+    Pizzas = association_proxy("restaurant_pizzas", "pizza")
+
 
     # add serialization rules
+    serialize_rules = ("-restaurant_pizzas.restaurant","-restaurant_pizzzas.pizza.restaurant_pizzas")
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -50,6 +54,12 @@ class RestaurantPizza(db.Model, SerializerMixin):
     price = db.Column(db.Integer, nullable=False)
 
     # add relationships
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+
+
+
+
 
     # add serialization rules
 
