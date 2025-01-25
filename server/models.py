4951e24310a9dc226bf2e1ecaaaa1c9db10cb166
Perpetual Akinyi <you@example.com>
2025-01-25 12:49:49 +0300

    added a relationship tu the RestaurantPizza

--- server/models.py
+++ server/models.py
@@ -54,16 +54,19 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"), nullable = False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"), nullable = False)
+
 
     # add relationships
-    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
-    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
-
+    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
+    pizza = db.relationship("Pizza", back_populates="restaurant_pizzas")
 
 
 
 
     # add serialization rules
+    serialize_rules = ("-restaurant.restaurant_pizzas", "-pizza.restaurant_pizzas")
 
     # add validation
 
